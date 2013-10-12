8148e6cbf02a3efc8e2e9a0a689f8d07282bf6ee
pipobizelli <pipo.bizelli@gmail.com>
2013-10-12 16:00:02 -0300

    node express e HTML inicial

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(1337, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:1337/');
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res){
+  res.send('hello world PORRA!!');
+});
+
+app.listen(1337);
+console.log("CONECTOU NA BAGAÇA!!!")
