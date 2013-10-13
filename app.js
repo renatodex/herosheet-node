3160e46d3c311d82067ab40b0e27b4b9b029cfa8
renatodex <renatodex@gmail.com>
2013-10-12 22:05:48 -0300

    Pointed to the production server;

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.set('views', __dirname + '/templates');
 app.use(express.bodyParser());
 
 app.get('/', function(req, res){
-	request('http://herosheet-backend:3000/api/list_team_heroes?team_id=1', 'GET', function(err, response, body) {
+	request('http://boiling-headland-1601.herokuapp.com:3000/api/list_team_heroes?team_id=1', 'GET', function(err, response, body) {
 		var json = JSON.parse(body)
 		res.render('index.ejs', {team: json['team']['nome'], herois: json['team']['herois']});
 	});
@@ -35,7 +35,7 @@ io.sockets.on('connection', function (socket) {
 						"&action_points=" + obj.action_points;
 
 		request({
-			url: 'http://herosheet-backend:3000/api/update_hero_sheet',
+			url: 'http://boiling-headland-1601.herokuapp.com:3000/api/update_hero_sheet',
 			body: str,
 			method: "POST"
 			},
